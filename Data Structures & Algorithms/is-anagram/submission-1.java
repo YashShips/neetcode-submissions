e867c64a5cdc150de87c537af666f1be81563625
Yash Yadav <yash.yadav.cse@gmail.com>
2026-06-15 02:40:20 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       if(s.length() != t.length()) return false;
+       Map<Character, Integer> map = new HashMap<>();
+       for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+       }
+       for(char ch : t.toCharArray()){
+            if(!map.containsKey(ch)){
+                return false;
+            }
+            map.put(ch, map.get(ch) - 1);
+            if(map.get(ch) == 0){
+                map.remove(ch);
+            }
+       }
+       return map.isEmpty();
+    }
+}
